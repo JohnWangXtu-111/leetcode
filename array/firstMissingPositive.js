afcbf21b4fea5954f69143462f72c8ea3e08b024
dsbJohn <johnwangxtu@gmail.com>
2022-06-30 16:35:50 +0800

    add

--- array/firstMissingPositive.js
+++ array/firstMissingPositive.js
@@ -12,3 +12,29 @@
 //   }
 //   return a;
 // };
+
+var firstMissingPositive = (nums) => {
+  //置换
+  for (let i = 0; i < nums.length; ++i) {
+    /**
+     * nums[i] > 0 && nums[i] < nums.length ===> 不能置换
+     * nums[nums[i] - 1] === nums[i]当前已经置换完毕了
+     * 重复元素会被跳过
+     */
+    while (
+      nums[i] > 0 &&
+      nums[i] < nums.length &&
+      nums[nums[i] - 1] !== nums[i]
+    ) {
+      let t = nums[i];
+      nums[i] = nums[nums[i] - 1];
+      nums[nums[i] - 1] = t;
+    }
+  }
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+  return nums.length + 1;
+};
